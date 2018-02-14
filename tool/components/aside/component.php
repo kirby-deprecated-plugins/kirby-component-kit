065fb706e724014de8954b4c96b053f10572c85d
Jens Törnell <jens.tornell@gmail.com>
2018-02-14 17:04:18 +0100

    Version number

--- tool/components/aside/component.php
+++ tool/components/aside/component.php
@@ -2,6 +2,11 @@
 namespace JensTornell\ComponentKit;
 ?>
 <aside>
-    <h1><a href="<?= $data['home']; ?>">Component Kit</a></h1>
+    <h1>
+        <a href="<?= $data['home']; ?>">
+            <span>Component Kit</span>
+            <div class="version">v0.1 beta</div>
+        </a>
+    </h1>
     <?= snippet('ckit/aside/item'); ?>
 </aside>
